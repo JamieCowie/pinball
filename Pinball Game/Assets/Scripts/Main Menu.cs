44fa6df65055afd4f97ed8dadf02e432aeb0a14c
JamieCowie <s4401607@glos.ac.uk>
2024-12-17 21:02:12 +0000

    all levels selectable and just needs maps

--- Pinball Game/Assets/Scripts/Main Menu.cs
+++ Pinball Game/Assets/Scripts/Main Menu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("LevelSelect");
     }
     public void Settings()
     {
@@ -20,5 +20,45 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+    public void Level1()
+    {
+        SceneManager.LoadScene("Map1");
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene("Map2");
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene("Map3");
+    }
+    public void Level4()
+    {
+        SceneManager.LoadScene("Map4");
+    }
+    public void Level5()
+    {
+        SceneManager.LoadScene("Map5");
+    }
+    public void Level6()
+    {
+        SceneManager.LoadScene("Map6");
+    }
+    public void Level7()
+    {
+        SceneManager.LoadScene("Map7");
+    }
+    public void Level8()
+    {
+        SceneManager.LoadScene("Map8");
+    }
+    public void Level9()
+    {
+        SceneManager.LoadScene("Map9");
+    }
+    public void Level10()
+    {
+        SceneManager.LoadScene("Map10");
     }
 }
